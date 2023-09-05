4e99a0a4ae77b9b428df9131dfe700bbab158a9b
Max Jacobson <max@hardscrabble.net>
2023-09-05 16:04:30 -0400

    Initial Commit

---
+++ Film School/Film_SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_SchoolApp.swift
+//  Film School
+//
+//  Created by Max Jacobson on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Film_SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
